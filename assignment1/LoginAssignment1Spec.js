55620bdfec4991e2564ce550a80df2ecae78546b
aameer-webonise <aameer.ausekar@weboniselab.com>
2016-03-09 10:23:55 +0530

    add more spec

--- assignment1/LoginAssignment1Spec.js
+++ assignment1/LoginAssignment1Spec.js
@@ -4,13 +4,34 @@ describe("Email Validation", function()
 		expect(validateEmail("aameer9@gmail.com")).toBeTruthy();
 		expect(validateEmail("aameer9gmail.com")).toBeFalsy();
 	});
+
+	it("Email should contain special symbols out of these (_.-)",function(){		
+		expect(validateEmail("aameer.ausekar@gmail.com")).toBeTruthy();
+		expect(validateEmail("aameer-ausekar@gmail.com")).toBeTruthy();
+		expect(validateEmail("aameer_ausekar@gmail.com")).toBeTruthy();
+		expect(validateEmail("aameer#ausekargmail.com")).toBeFalsy();
+		expect(validateEmail("aameer^%gmail.com")).toBeFalsy();
+		expect(validateEmail("aam!*er#ausekargmail.com")).toBeFalsy();
+	});
 });
 
 describe("Password Validation", function() 
 {
-	it("Password should be having one alphabet, number and a special character.",function(){		
+	it("Password should be having at least one alphabet",function(){		
 		expect(validatePassword("aameer9@")).toBeTruthy();
-		expect(validatePassword("aa8_me")).toBeTruthy();
-		expect(validatePassword("aa8me")).toBeFalsy();
+		expect(validatePassword("7-")).toBeFalsy();
+		expect(validatePassword("954#")).toBeFalsy();
+	});
+
+	it("Password should be having at least one number",function(){		
+		expect(validatePassword("aameer9@")).toBeTruthy();
+		expect(validatePassword("john6%")).toBeTruthy();
+		expect(validatePassword("mack")).toBeFalsy();
+	});
+
+	it("Password should be having at least one special symbol out of these (!.@#$%^&*()_)",function(){		
+		expect(validatePassword("aameer9@")).toBeTruthy();
+		expect(validatePassword("kitty$9")).toBeTruthy();
+		expect(validatePassword("mack8")).toBeFalsy();
 	});
 });
--- assignment2/LoginSpec.js
+++ assignment2/LoginSpec.js
@@ -31,14 +31,35 @@ describe("Email & Password Validation", function()
 
     });
 
-
 	it("Email should have @",function(){		
 		expect("aameer9@gmail.com").isValidEmail();
 		expect("aameer9gmail.com").not.isValidEmail();
 	});
 
-	it("Password should be having one alphabet, number and a special character.",function(){		
-		expect("qjhv4_").isValidPassword();
+	it("Email should contain special symbols out of these (_.-)",function(){		
+		expect("aameer.ausekar@gmail.com").isValidEmail();
+		expect("aameer-ausekar@gmail.com").isValidEmail();
+		expect("aameer_ausekar@gmail.com").isValidEmail();
+		expect("aameer#ausekargmail.com").not.isValidEmail();
+		expect("aameer^%gmail.com").not.isValidEmail();
+		expect("aam!*er#ausekargmail.com").not.isValidEmail();
+	});
+	it("Password should be having at least one alphabet",function(){		
+		expect("aameer9@").isValidPassword();
+		expect("7-").not.isValidPassword();
+		expect("954#").not.isValidEmail();
+	});
+
+	it("Password should be having at least one number",function(){		
+		expect("aameer9@").isValidPassword();
+		expect("john6%").isValidPassword();
+		expect("mack").not.isValidPassword();
+	});
+
+	it("Password should be having at least one special symbol out of these (!.@#$%^&*()_)",function(){		
+		expect("aameer9@").isValidPassword();
+		expect("kitty$9").isValidPassword();
+		expect("mack8").not.isValidPassword();
 	});
 });
 
