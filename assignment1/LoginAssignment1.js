30ca9613f54bbcb71ba6791d13997db995f8549d
aameer.ausekar@weboniselab.com <aameer.ausekar@weboniselab.com>
2016-03-11 18:30:47 +0530

    commit

--- assignment1/LoginAssignment1.js
+++ assignment1/LoginAssignment1.js
@@ -1,12 +1,11 @@
+var emailRegularExpression = new RegExp("^[a-zA-Z]+[A-Za-z0-9+_.-]+@([a-zA-Z]+)([.]+)([a-zA-Z]+)$");
+ var passwordPatt=new RegExp(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]+$/);  
 function validateEmail(email)
 {
-  var emailPatt = new RegExp("^[a-zA-Z]+[A-Za-z0-9+_.-]+@([a-zA-Z]+)([.]+)([a-zA-Z]+)$");
   return emailPatt.test(email);
 }
 
 function validatePassword(password)
 {
-   //var passwordPatt=new RegExp("^([a-zA-Z0-9]+[_!-.@#$%^&\\s]+[a-zA-Z0-9]*)+$");
-   var passwordPatt=new RegExp(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]+$/);  
    return passwordPatt.test(password);
 }
